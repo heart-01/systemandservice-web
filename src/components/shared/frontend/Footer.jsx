c9ad3b1eae271ca8e91231f2297f232dc6841701
Siwat Jermwatthana <heart.siwat@hiveground.com>
2023-02-28 14:46:11 +0700

    Change link facebook and line

--- src/components/shared/frontend/Footer.jsx
+++ src/components/shared/frontend/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
       <div className="bg-blue-500 p-10">
         <div className="flex justify-center align-center justify-items-center">
           <Image width={700} preview={false} src={`${process.env.REACT_APP_PUBLIC_URL}/assets/images/footer-1.png`} />
-          <a href="https://me-qr.com/2554992" target="_blank">
+          <a href="https://www.facebook.com/profile.php?id=100064105025273" target="_blank">
             <Image width={200} preview={false} src={`${process.env.REACT_APP_PUBLIC_URL}/assets/images/footer-2.jpg`} />
           </a>
-          <a href="https://me-qr.com/2554922" target="_blank">
+          <a href="https://linevoom.line.me/user/_dV7quACNaI5CvGKpgCDIcpNBCHpUdQWhX2K9L68?utm_medium=windows&utm_source=desktop&utm_campaign=Profile" target="_blank">
             <Image width={200} preview={false} src={`${process.env.REACT_APP_PUBLIC_URL}/assets/images/footer-3.jpg`} />
           </a>
         </div>
